3cc055a7318e9dc2c3beb54e7d774c7fb86dc59f
Edicson Abel <edicsonabelinfo@gmail.com>
2024-08-05 19:42:43 -0400

    Add  new colors and header for navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['content/**/*.md', 'layouts/**/*.html'],
   theme: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      'blue-dark': 'var(--color-blue-dark)'
+    },
     extend: {}
   },
   plugins: []
